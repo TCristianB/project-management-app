7040db1337b4cbe11f45a4f41f97bf92c70318c5
TCristianB <ariwachi@gmail.com>
2021-10-17 18:40:09 -0300

    Setting heroku

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ app.use('/api/projects', projectsRouter)
 
 app.use('/api/tickets', ticketsRouter)
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const server = app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
 
 module.exports = { app, server }
